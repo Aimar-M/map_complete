c8811edf4ff56a77fc27fe52aba0a1026a44b773
Aimar Muhirwa <mcyusaaimar@gmail.com>
2024-05-09 11:22:09 +0200

    map complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import {Icon} from "leaflet";
+import "leaflet/dist/leaflet.css"
+
 
 function App() {
+    const markers = [
+        {
+            geocode: [48.86, 2.3522],
+            popup: "pharmacy continental"
+        },
+        {
+            geocode: [48.85, 2.3522],
+            popup: "other pharmacy"
+
+        }
+    ]
+
+    const customIcon= new Icon({
+            iconUrl: require("./placeholder.png"),
+            iconSize: [38, 38]
+    }
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // coordinates for rwanda: [-1.9146409839831982, 30.05164869832044]
+      <MapContainer center={[48.8566, 2.3522]} zoom={12.5}>
+          <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {markers.map(marker => {
+              <Marker position={marker.geocode}></Marker>
+          })}
+
+      </MapContainer>
   );
 }
 
