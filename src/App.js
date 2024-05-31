974dff47b139abc2a7721347f1ee3936bfc87643
Aimar Cyusa Muhirwa <mcyusaaimar@gmail.com>
2024-05-31 16:44:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import "leaflet/dist/leaflet.css"
 import facilitiesData from "./assets/data.json"
 
 function App() {
-    const markers = [
-        {
-            geocode: [48.8566, 2.3522],
-            popup: "pharmacy continental"
-        },
-        {
-            geocode: [48.85, 2.3522],
-            popup: "other pharmacy"
+    // const markers = [
+    //     {
+    //         geocode: [48.8566, 2.3522],
+    //         popup: "pharmacy continental"
+    //     },
+    //     {
+    //         geocode: [48.85, 2.3522],
+    //         popup: "other pharmacy"
 
-        }
-    ]
+    //     }
+    // ]
 
-    const customIcon= new Icon({
-            iconUrl: require("./placeholder.png"),
-            iconSize: [38, 38]
-    }
-    )
+    // const customIcon= new Icon({
+    //         iconUrl: require("./placeholder.png"),
+    //         iconSize: [38, 38]
+    // }
+    // )
 
   return (
       // coordinates for rwanda: [-1.9146409839831982, 30.05164869832044]
@@ -32,9 +32,9 @@ function App() {
           />
 
           <Marker position={[48.8566, 2.3522]} icon={customIcon}> </Marker>
-          {markers.map(marker => {
-              return <Marker position={marker.geocode} icon={customIcon}></Marker>
-          })}
+          // {markers.map(marker => {
+          //     return <Marker position={marker.geocode} icon={customIcon}></Marker>
+          // })}
           {facilitiesData.map(marker => {
               return <Marker position={[marker.latitude, marker.longitude]} icon={customIcon}>
                   <Popup>
