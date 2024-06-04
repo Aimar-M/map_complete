8f1c5ddc28e8ea970b67e3cb5352df7f74fcb146
Demmythetechie <demmythetechie@gmail.com>
2024-06-04 10:46:37 +0100

    updated

--- src/map.js
+++ src/map.js
@@ -12,7 +12,7 @@ function Map() {
 
     const customIcon = new Icon({
         iconUrl: require('./placeholder.png'),
-        iconSize: [15, 15]
+        iconSize: [20, 20]
     });
 
     return (
@@ -25,8 +25,25 @@ function Map() {
 
             {coOrdinates.map((location)=>(
                 <Marker position={[location.latitude, location.longitude]} icon={customIcon} >
-                    <Popup>
-                        {location.name}
+                    <Popup className='mapInfo'>
+                        <p>{location.name}</p>
+                        <div className='starMap'>
+                            {Array.from({ length: location.Ratings }, (_, i) => i).map(()=> (
+                                <div>
+                                    <svg viewBox="0 0 28 28" fill="none">
+                                        <path d="M13.9999 2.33337L17.6049 9.63671L25.6666 10.815L19.8333 16.4967L21.2099 24.5234L13.9999 20.7317L6.78992 24.5234L8.16659 16.4967L2.33325 10.815L10.3949 9.63671L13.9999 2.33337Z" stroke="#EAB308" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                </div>
+                            ))}
+                            {Array.from({ length: 5 - location.Ratings }, (_, i) => i).map(()=> (
+                                <div>
+                                    <svg viewBox="0 0 28 28" fill="none">
+                                        <path d="M13.9999 2.33337L17.6049 9.63671L25.6666 10.815L19.8333 16.4967L21.2099 24.5234L13.9999 20.7317L6.78992 24.5234L8.16659 16.4967L2.33325 10.815L10.3949 9.63671L13.9999 2.33337Z" stroke="#999999" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                </div>
+                            ))}
+                        </div>
+                        <p>{location.address}</p>
                     </Popup>
                 </Marker>
             ))}
